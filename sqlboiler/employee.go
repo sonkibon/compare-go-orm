69c5f12097b214cdcc4c25ccf956af72235ada21
Kwibong Song <kwibong0128@gmail.com>
2023-05-19 20:28:41 +0900

    feat: implement find method

--- sqlboiler/employee.go
+++ sqlboiler/employee.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/sonkibon/compare-go-orm/model"
 	"github.com/sonkibon/compare-go-orm/repository"
+	"github.com/sonkibon/compare-go-orm/sqlboiler/entity"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
@@ -11,4 +16,20 @@ type employeeRepositorImpl struct {
 
 func NewEmployeeRepositorImpl(exec boil.ContextExecutor) repository.Employee {
 	return &employeeRepositorImpl{exec: exec}
+}
+
+func (e *employeeRepositorImpl) Find(ctx context.Context, empNo int) (*model.Employee, error) {
+	employee, err := entity.FindEmployee(ctx, e.exec, empNo)
+	if err != nil {
+		return nil, fmt.Errorf("entity.FindEmployee: %w", err)
+	}
+
+	return &model.Employee{
+		EmpNo:     employee.EmpNo,
+		BirthDate: employee.BirthDate,
+		FirstName: employee.FirstName,
+		LastName:  employee.LastName,
+		Gender:    model.Gender(employee.Gender),
+		HireDate:  employee.HireDate,
+	}, nil
 }
