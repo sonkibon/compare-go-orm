58eef55dc95f53307d42189b246363bfa070886d
Kwibong Song <kwibong0128@gmail.com>
2023-05-20 22:40:34 +0900

    feat: define dept manager schema

--- ent/schema/deptmanager.go
+++ ent/schema/deptmanager.go
@@ -1,6 +1,13 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
 
 // DeptManager holds the schema definition for the DeptManager entity.
 type DeptManager struct {
@@ -9,10 +16,18 @@ type DeptManager struct {
 
 // Fields of the DeptManager.
 func (DeptManager) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.Time("from_date").SchemaType(map[string]string{dialect.MySQL: "date"}),
+		field.Time("to_date").SchemaType(map[string]string{dialect.MySQL: "date"}),
+		field.Time("created_at").SchemaType(map[string]string{dialect.MySQL: "datetime"}).Default(time.Now),
+		field.Time("updated_at").SchemaType(map[string]string{dialect.MySQL: "datetime"}).Default(time.Now),
+	}
 }
 
 // Edges of the DeptManager.
 func (DeptManager) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.From("employee", Employee.Type).Ref("dept_manager").Unique().Required(),
+		edge.From("department", Department.Type).Ref("dept_manager").Unique().Required(),
+	}
 }
