640172f16287c4056dc44c5fb006730e3bb3e3ed
Kwibong Song <kwibong0128@gmail.com>
2023-05-20 22:40:06 +0900

    feat: define department schema

--- ent/schema/department.go
+++ ent/schema/department.go
@@ -1,6 +1,13 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
 
 // Department holds the schema definition for the Department entity.
 type Department struct {
@@ -9,10 +16,16 @@ type Department struct {
 
 // Fields of the Department.
 func (Department) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("dept_name").SchemaType(map[string]string{dialect.MySQL: "varchar(40)"}).Unique(),
+		field.Time("created_at").SchemaType(map[string]string{dialect.MySQL: "datetime"}).Default(time.Now),
+		field.Time("updated_at").SchemaType(map[string]string{dialect.MySQL: "datetime"}).Default(time.Now),
+	}
 }
 
 // Edges of the Department.
 func (Department) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.To("dept_manager", DeptManager.Type),
+	}
 }
