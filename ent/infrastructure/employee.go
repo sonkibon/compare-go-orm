c348559006709dc40b78b348644dd429db041458
Kwibong Song <kwibong0128@gmail.com>
2023-05-20 22:46:28 +0900

    feat(ent): implement find method

--- ent/infrastructure/employee.go
+++ ent/infrastructure/employee.go
@@ -1,7 +1,12 @@
 package infrastructure
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/sonkibon/compare-go-orm/ent"
+	emp "github.com/sonkibon/compare-go-orm/ent/employee"
+	"github.com/sonkibon/compare-go-orm/model"
 	"github.com/sonkibon/compare-go-orm/repository"
 )
 
@@ -11,4 +16,23 @@ type employeeRepositorImpl struct {
 
 func NewEmployeeRepositorImpl(client *ent.Client) repository.Employee {
 	return &employeeRepositorImpl{client: client}
+}
+
+func (e *employeeRepositorImpl) Find(ctx context.Context, empNo int) (*model.Employee, error) {
+	employee, err := e.client.Employee.
+		Query().
+		Where(emp.IDEQ(empNo)).
+		Only(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("e.client.Employee.Query.Where.Only: %w", err)
+	}
+
+	return &model.Employee{
+		EmpNo:     employee.ID,
+		BirthDate: employee.BirthDate,
+		FirstName: employee.FirstName,
+		LastName:  employee.LastName,
+		Gender:    model.Gender(employee.Gender),
+		HireDate:  employee.HireDate,
+	}, nil
 }
