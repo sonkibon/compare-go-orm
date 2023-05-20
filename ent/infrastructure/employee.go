52fba48500825587ef13507f3c4a49bfa535ce45
Kwibong Song <kwibong0128@gmail.com>
2023-05-20 22:45:57 +0900

    feat(ent): define employee repositor implementation

--- ent/infrastructure/employee.go
+++ ent/infrastructure/employee.go
@@ -1,1 +1,14 @@
 package infrastructure
+
+import (
+	"github.com/sonkibon/compare-go-orm/ent"
+	"github.com/sonkibon/compare-go-orm/repository"
+)
+
+type employeeRepositorImpl struct {
+	client *ent.Client
+}
+
+func NewEmployeeRepositorImpl(client *ent.Client) repository.Employee {
+	return &employeeRepositorImpl{client: client}
+}
