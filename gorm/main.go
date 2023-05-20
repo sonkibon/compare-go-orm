4bfb616e0da41d22659a35e19ff1674b53d64b91
Kwibong Song <kwibong0128@gmail.com>
2023-05-20 17:42:37 +0900

    feat: add migration proccess

--- gorm/main.go
+++ gorm/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+	"github.com/sonkibon/compare-go-orm/gorm/infrastructure"
+)
+
+func main() {
+	dsn := "user:password@tcp(127.0.0.1:3306)/test_db?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("gorm.Open: %v", err)
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&infrastructure.Employee{})
+}
