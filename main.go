7b0e9d4a4f0c5a7257c80bf93fb1d4239481265a
Kwibong Song <kwibong0128@gmail.com>
2023-05-20 22:49:14 +0900

    feat(ent): connect to database and migrate schema

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/sonkibon/compare-go-orm/ent"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	client, err := ent.Open("mysql", "user:password@tcp(localhost:3306)/test_db?parseTime=True")
+	if err != nil {
+		log.Fatalf("Open: %v", err)
+	}
+	defer client.Close()
+
+	ctx := context.Background()
+
+	if err := client.Schema.Create(ctx); err != nil {
+		log.Fatalf("client.Schema.Create: %v", err)
+	}
 }
